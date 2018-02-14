3e49ea6572aba80cd31e237d5429f4bb2a06b655
Ludonope <ludovic.petrenko@epitech.eu>
2018-02-14 18:46:04 +0100

    Added support for raw input

--- core/src/HttpRequestParser.hpp
+++ core/src/HttpRequestParser.hpp
@@ -6,4 +6,6 @@ namespace zia::http
 {
 	api::HttpRequest parseRequest(std::string_view input);
 	api::HttpRequest parseRequest(std::string_view input, std::size_t &bodyLen);
+	api::HttpRequest parseRequest(std::byte const *input);
+	api::HttpRequest parseRequest(std::byte const *input, std::size_t &bodyLen);
 }
