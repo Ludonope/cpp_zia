90217c2d9b34c44c5d5ca45092b52fb67628d054
Ludonope <ludovic.petrenko@epitech.eu>
2018-02-24 18:25:47 +0100

    Changed wrong names in benchmarks

--- common/test/bench/ResponseSerializer.cpp
+++ common/test/bench/ResponseSerializer.cpp
@@ -4,7 +4,7 @@
 #include <ctime>
 #include "ResponseSerializer.hpp"
 
-static void BM_HttpRequestParser_VeryShortResponse(benchmark::State& state) {
+static void BM_ResponseSerializer_VeryShortResponse(benchmark::State& state) {
 	auto res = zia::api::HttpResponse{
 		zia::api::http::Version::http_1_1,
 		{ },
@@ -17,10 +17,10 @@ static void BM_HttpRequestParser_VeryShortResponse(benchmark::State& state) {
 		auto req = zia::http::responseToString(res);
 	}
 }
-BENCHMARK(BM_HttpRequestParser_VeryShortResponse);
+BENCHMARK(BM_ResponseSerializer_VeryShortResponse);
 
 template <std::size_t ContentSize>
-static void BM_HttpRequestParser_ContentResponse(benchmark::State& state) {
+static void BM_ResponseSerializer_ContentResponse(benchmark::State& state) {
 	auto body = std::vector<std::byte>{};
 	
 	std::srand(std::time(nullptr));
@@ -46,29 +46,29 @@ static void BM_HttpRequestParser_ContentResponse(benchmark::State& state) {
 		auto req = zia::http::responseToString(res);
 	}
 }
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 0);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 1);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 2);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 4);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 8);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 16);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 32);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 64);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 128);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 256);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 512);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 1024);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 2048);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 4096);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 8192);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 16384);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 32768);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 65536);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 131072);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 262144);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 524288);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 1048576);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 2097152);
-BENCHMARK_TEMPLATE(BM_HttpRequestParser_ContentResponse, 4194304);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 0);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 1);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 2);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 4);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 8);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 16);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 32);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 64);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 128);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 256);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 512);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 1024);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 2048);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 4096);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 8192);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 16384);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 32768);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 65536);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 131072);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 262144);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 524288);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 1048576);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 2097152);
+BENCHMARK_TEMPLATE(BM_ResponseSerializer_ContentResponse, 4194304);
 
 BENCHMARK_MAIN();
