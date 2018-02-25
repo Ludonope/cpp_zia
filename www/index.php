3f2408bb37885809888dff7043efcb469aa90b8e
Ludonope <ludovic.petrenko@epitech.eu>
2018-02-25 23:34:01 +0100

    Made some minor changes

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Welcome</title>
+</head>
+<body>
+<div>
+<?php
+  $fileLink = 'hello.php';
+  $textLink = 'Welcome !';
+
+  echo "<a href=$fileLink>$textLink</a>";
+?>
+</div>
+<div>
+<a href="yolo/">Subfolder</a>
+</div>
+</body>
+</html>
